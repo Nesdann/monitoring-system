e8ae3cca96ee3e4bff5e2e4ecf3119f5b86ff1c6
Ale <danielrojas4411@gmail.com>
2026-05-29 20:35:54 -0300

    implement initial agent-backend communication protocol

---
+++ protocol/protocol.go
@@ -0,0 +1,64 @@
+package protocol
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"io"
+)
+
+func WriteMessage(w io.Writer, msg Message) error {
+	//w io.writer es cualquier cosa donde pueda escribir, como un socket o un archivo
+
+	payload, err := json.Marshal(msg)//lo pasa a json, lo convierte a bytes
+	if err != nil {
+		return err
+	}
+
+	length := uint32(len(payload))
+	//le dice al receptor cuantos bytes va a recibir, para que sepa cuando termina el mensaje
+	//protocolo de longitud-prefijada, primero se envía la longitud del mensaje y luego el mensaje en sí
+
+	err = binary.Write(
+		//escribimos en el buffer la longitud del mensaje, con un orden de bytes específico (big endian)
+		w,
+		binary.BigEndian,
+		length,
+	)
+
+	if err != nil {
+		return err
+	}
+	//luego escribimos el mensaje en sí
+	_, err = w.Write(payload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadMessage(r io.Reader)(Message, error){
+	var length uint32
+	var msg Message
+	//leer solo la longitud del mensaje
+	length:= make([]byte, 4)
+	_, err := io.ReadFull(r, length)//ler exactamente 4 bytes, que es el tamaño de uint32
+	if err != nil {
+		return msg, err
+	}
+	payload:= make([]byte, binary.BigEndian.Uint32(length))
+	//crea un buffer del tamaño del mensaje, para leer el mensaje completo
+	_, err = io.ReadFull(r, payload)
+	if err != nil {
+		return msg, err
+	}
+
+
+	err = json.Unmarshal(payload, &msg)//parsea el mnensaje
+	if err != nil {
+		return msg, err
+	}
+
+	return msg, nil
+
+}
